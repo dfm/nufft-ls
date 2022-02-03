02f398e6ba3ce4d53bcfdc89bd738fdf6a5a08e4
Dan F-M <foreman.mackey@gmail.com>
2022-02-02 20:49:37 -0500

    adding some logic to handle different platforms and compilers

--- setup.py
+++ setup.py
@@ -1,45 +1,65 @@
 from glob import glob
 
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension, build_ext, MACOS, WIN, has_flag
 
-compile_link_args = ['-march=native',
-                     '-Ofast',
-                     '-fopenmp',
-                     #'-funroll-loops',
-                     
-                     # icc
-                     #'-march=core-avx2',  # for rome on icc
-                     #'-fp-model', 'strict',
-
-                     # clang
-                     #'-stdlib=libc++',
-                     #'-fveclib=SVML',
-                     #'-lsvml'
-
-                     # aocc
-                     #'-std=c++11',
-                     #'-ffast-math',
-                     #'-lamdlibm',
-                     #'-lm',
-                     ]
 
 ext_modules = [
     Pybind11Extension(
         "nufft_ls",
         sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
-        include_dirs=['include/'],
-        extra_compile_args=compile_link_args,
-        extra_link_args=compile_link_args,
-        language='c++',
+        include_dirs=["include/"],
+        language="c++",
     ),
 ]
 
-setup(name="nufft_ls",
+
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        # Handle platform specific optimization flags
+        if WIN:
+            # I don't know what optimization flags would be right on Windows
+            pass
+        else:
+            cflags = []
+            ldflags = []
+            for flag in [
+                "-march=native",
+                "-Ofast",
+                "-fopenmp",
+                "-funroll-loops",
+                "-ffast-math",
+            ]:
+                if has_flag(self.compiler, flag):
+                    cflags.append(flag)
+
+            # None of the rest of these will work on Mac
+            if not MACOS:
+                cc = self.compiler.compiler_so[:2].join(" ")
+                if "icc" in cc:
+                    if has_flag(self.compiler, "-march=core-avx2"):
+                        cflags += ["-march=core-avx2"]
+                    cflags += ["-fp-model", "strict"]
+                elif "clang" in cc:
+                    cflags += ["-fveclib=SVML"]
+                    ldflags += ["-lsvml"]
+                elif "aocc" in cc:
+                    ldflags += ["-lamdlibm", "-lm"]
+
+            for ext in self.extensions:
+                ext.extra_compile_args += cflags
+                ext.extra_link_args += ldflags
+
+        build_ext.build_extensions(self)
+
+
+setup(
+    name="nufft_ls",
     version="0.0.1",
     url="https://github.com/dfm/nufft-ls",
     zip_safe=False,
     python_requires=">=3.6",
+    install_requires=["numpy", "astropy", "threadpoolctl"],
     ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
+    cmdclass={"build_ext": custom_build_ext},
 )
