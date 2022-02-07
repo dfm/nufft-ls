a46616d58d1daea471d590c0f9926d04c031ddbb
Lehman Garrison <lgarrison@flatironinstitute.org>
2022-02-07 13:56:27 -0500

    Refactor into src layout in prep for GPU

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from glob import glob
 
 from setuptools import setup
+import pybind11.setup_helpers
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 compile_link_args = ['-march=native',
                      '-Ofast',
                      '-fopenmp',
-                     #'-funroll-loops',
+                     #'-unroll',
                      
                      # icc
                      #'-march=core-avx2',  # for rome on icc
@@ -24,16 +25,12 @@ compile_link_args = ['-march=native',
                      #'-lm',
                      ]
 
-ext_modules = [
-    Pybind11Extension(
-        "nufft_ls",
-        sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
-        include_dirs=['include/'],
-        extra_compile_args=compile_link_args,
-        extra_link_args=compile_link_args,
-        language='c++',
-    ),
-]
+ext_modules = pybind11.setup_helpers.intree_extensions(["src/nufft_ls/cpu.cpp"])
+
+ext_modules[0].include_dirs = ['include/']
+ext_modules[0].extra_compile_args = compile_link_args
+ext_modules[0].extra_link_args = compile_link_args
+ext_modules[0].language = 'c++'
 
 setup(name="nufft_ls",
     version="0.0.1",
@@ -42,4 +39,5 @@ setup(name="nufft_ls",
     python_requires=">=3.6",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
+    package_dir={'':'src'},
 )
