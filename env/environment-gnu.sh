ae5ed0f1edd005a491ba91e8f028b42a29403d4f
Lehman Garrison <lgarrison@flatironinstitute.org>
2024-01-04 13:44:18 -0500

    build: migrate from setuptools to scikit-build-core

--- env/environment-gnu.sh
+++ env/environment-gnu.sh
@@ -1,13 +1,6 @@
-module load cmake/3
-module load gcc/11
-module load python/3.9
+ml modules/2.2
+module load cmake
+module load gcc
+module load python
 
 . venv/bin/activate
-
-export CC=gcc
-export CXX=g++
-
-# only required for 'python setup.py develop'
-# because `pip install -e .` doesn't seem to use these vars...?
-export LDSHARED="$CC -L$(python3-config --prefix)/lib -shared"
-export LDCXXSHARED="$CXX -L$(python3-config --prefix)/lib -shared"
