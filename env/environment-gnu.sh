acb0772dac13943636f08054580c8d565fd57f0c
Lehman Garrison <lgarrison@flatironinstitute.org>
2024-01-19 18:06:09 -0500

    cmake: add cuda support

--- env/environment-gnu.sh
+++ env/environment-gnu.sh
@@ -1,6 +1,9 @@
 ml modules/2.2
-module load cmake
-module load gcc
-module load python
+ml cmake
+ml gcc
+ml python
+ml cuda
+
+export CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=75"
 
 . venv/bin/activate
